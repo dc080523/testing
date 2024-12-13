8d22f7eba58f749dcc4897a25c28ecbea0ee67f1
dc080523 <dominicchristiandelacruz@yahoo.com>
2024-12-13 21:23:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -238,5 +238,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(Server is running on http://localhost:${port});
+    console.log(Server is running on http://localhost:${port}');
 });
