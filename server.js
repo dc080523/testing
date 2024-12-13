285bfcaffa4c9abf6a474693ab056655b9cd399d
dc080523 <dominicchristiandelacruz@yahoo.com>
2024-12-13 20:04:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.use(session({
     secret: 'secretkey',
     resave: false,
     saveUninitialized: true,
+    cookie: { secure: false }  // Make sure this is false for development, true if using HTTPS
 }));
 
 // Middleware to check if the user is an admin
