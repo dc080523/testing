4765196960dd5dc859a786b1ef8f33904a9b5f42
dc080523 <dominicchristiandelacruz@yahoo.com>
2024-12-13 15:01:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -140,4 +140,10 @@ app.get('/check-admin-session', (req, res) => {
 
 // Home route (For checking that the server is up)
 app.get('/', (req, res) => {
-    res.send('Server is up 
+    res.send('Server is up and running!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
